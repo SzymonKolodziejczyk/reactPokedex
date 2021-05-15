daae1d9b630240d1ddeeda4371269bebbc05807d
Szymon Kołodziejczyk <szymonnkolodziejczyk@gmail.com>
2021-05-15 02:54:59 +0200

    Updates

---
+++ precache-manifest.d50f5c58e65aa04521af0da1c61832c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362e4351187cb48ce207f20d49815640",
+    "url": "/reactPokedex/index.html"
+  },
+  {
+    "revision": "aff0536e1b754a7df2fe",
+    "url": "/reactPokedex/static/css/2.108f5224.chunk.css"
+  },
+  {
+    "revision": "aff0536e1b754a7df2fe",
+    "url": "/reactPokedex/static/js/2.1e578322.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactPokedex/static/js/2.1e578322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cc0d9e087c18a6f9a4",
+    "url": "/reactPokedex/static/js/main.e87cd377.chunk.js"
+  },
+  {
+    "revision": "943cb4066a90e6f2e939",
+    "url": "/reactPokedex/static/js/runtime-main.64de4d2f.js"
+  },
+  {
+    "revision": "a23859603353e6054fc47b7c41545351",
+    "url": "/reactPokedex/static/media/bg-large.a2385960.png"
+  }
+]);
